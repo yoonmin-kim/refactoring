a77a9526cdf836412eddb663fd49f9037675ca4a
yoonmin-kim <rladbsals23@gmail.com>
2022-04-09 22:09:22 +0900

    refactor : 코드 정리하기, Slide Statements
    · 관련있는 코드끼리 묶여 있어야 코드를 더 쉽게 이해할 수 있다
    · 함수에서 사용할 변수를 상단에 미리 정의하기 보다는, 해당 변수를 사용하는 코드
    바로위에 선언하자
    · 관련있는 코드끼리 묶은 다음, 함수 추출하기 (Extract Function)를 사용해서 더
    깔끔하게 분리할 수도 있다

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_05_slide_statements/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_05_slide_statements/StudyDashboard.java
@@ -1,40 +1,36 @@
 package me.whiteship.refactoring._02_duplicated_code._05_slide_statements;
 
-import org.kohsuke.github.GHIssue;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
 public class StudyDashboard {
 
     private void printParticipants(int eventId) throws IOException {
-        // Get github issue to check homework
+        // 코드가 정리된 부분
         GitHub gitHub = GitHub.connect();
         GHRepository repository = gitHub.getRepository("whiteship/live-study");
         GHIssue issue = repository.getIssue(eventId);
 
-        // Get participants
         Set<String> participants = new HashSet<>();
         issue.getComments().forEach(c -> participants.add(c.getUserName()));
 
-        // Print participants
         participants.forEach(System.out::println);
     }
 
     private void printReviewers() throws IOException {
-        // Get github issue to check homework
+        // 코드가 정리되지 않은 부분
+        Set<String> reviewers = new HashSet<>();
         GitHub gitHub = GitHub.connect();
         GHRepository repository = gitHub.getRepository("whiteship/live-study");
         GHIssue issue = repository.getIssue(30);
 
-        // Get reviewers
-        Set<String> reviewers = new HashSet<>();
         issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
 
-        // Print reviewers
         reviewers.forEach(System.out::println);
     }
 
