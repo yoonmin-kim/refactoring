da6b5eabbfc974c6f917f5e4815fc83bf0407484
yoonmin-kim <rladbsals23@gmail.com>
2022-04-14 21:11:03 +0900

    refactor : 조건문 분해하기, Decompose Conditional
    · 여러 조건에 따라 달라지는 코드를 작성하다 보면 종종 긴 함수가 만들어지는 것을
    목격할 수 있다
    · "조건"과 "액션" 모두 "의도"를 표현해야 한다
    · 기술적으로는 "함수 추출하기"와 동일한 리팩토링이지만 의도만 다를 뿐이다

--- src/main/java/me/whiteship/refactoring/_03_long_function/_11_decompose_conditional/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_11_decompose_conditional/StudyDashboard.java
@@ -1,16 +1,16 @@
 package me.whiteship.refactoring._03_long_function._11_decompose_conditional;
 
-import org.kohsuke.github.GHIssue;
-import org.kohsuke.github.GHIssueComment;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
 public class StudyDashboard {
 
@@ -62,15 +62,26 @@ public class StudyDashboard {
     }
 
     private Participant findParticipant(String username, List<Participant> participants) {
-        Participant participant;
-        if (participants.stream().noneMatch(p -> p.username().equals(username))) {
-            participant = new Participant(username);
-            participants.add(participant);
-        } else {
-            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
-        }
+        return isNewParticipant(username, participants) ?
+            createNewParticipant(username, participants) :
+            findExistingParticipant(username, participants);
+    }
 
+    private Participant findExistingParticipant(String username, List<Participant> participants) {
+        Participant participant;
+        participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
         return participant;
+    }
+
+    private Participant createNewParticipant(String username, List<Participant> participants) {
+        Participant participant;
+        participant = new Participant(username);
+        participants.add(participant);
+        return participant;
+    }
+
+    private boolean isNewParticipant(String username, List<Participant> participants) {
+        return participants.stream().noneMatch(p -> p.username().equals(username));
     }
 
 }
