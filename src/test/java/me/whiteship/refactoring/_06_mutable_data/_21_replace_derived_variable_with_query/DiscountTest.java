c6dfe232eafa1bd774b78850d1600c29258608f6
yoonmin-kim <rladbsals23@gmail.com>
2022-04-24 22:42:47 +0900

    refactor : 파생 변수를 질의 함수로 바꾸기, Replace Derived Variable with Query
    · 변경할 수 있는 데이터를 최대한 줄이도록 노력해야 한다
    · 계산해서 알아낼 수 있는 변수는 제거할 수 있다
     · 계산 자체가 데이터의 의미를 잘 표현하는 경우도 있다
     · 해당 변수가 어디선가 잘못된 값으로 수정될 수 있는 가능성을 제거할 수 있다
    · 계산에 필요한 데이터가 변하지 않는 값이라면, 계산의 결과에 해당하는 데이터
    역시 불변 데이터기 때문에 해당 변수는 그대로 유지할 수 있다

--- src/test/java/me/whiteship/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/DiscountTest.java
+++ src/test/java/me/whiteship/refactoring/_06_mutable_data/_21_replace_derived_variable_with_query/DiscountTest.java
@@ -1,15 +1,15 @@
 package me.whiteship.refactoring._06_mutable_data._21_replace_derived_variable_with_query;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class DiscountTest {
 
     @Test
     void discount() {
         Discount discount = new Discount(100);
-//        assertEquals(100, discount.getDiscountedTotal());
+        assertEquals(100, discount.getDiscountedTotal());
 
         discount.setDiscount(10);
         assertEquals(90, discount.getDiscountedTotal());
