e81035b94e83d776fa2e00116ddd3007878e237d
yoonmin-kim <rladbsals23@gmail.com>
2022-04-25 22:22:07 +0900

    refactor : 여러 함수를 변환 함수로 묶기, Combine Functions into Transform
    · 관련있는 여러 파생 변수를 만들어내는 함수가 여러곳에서 만들어지고 사용된다면
    그러한 파생 변수를 "변환 함수(transform function)" 를 통해 한 곳으로 모아둘
    수 있다
    
    · 소스 데이터가 변결될 수 있는 경우에는 "여러 함수를 클래스로 묶기(Combine Functions into Class)"
    를 사용하는 것이 적절하다
    
    · 소스 데이터가 변경되지 않는 경우에는 두 가지 방법을 모두 사용할 수 있지만, 변환
    함수를 사용해서 불변 데이터의 필드로 생성해 두고 재사용할 수도 있다

--- src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Client1.java
+++ src/main/java/me/whiteship/refactoring/_06_mutable_data/_22_combine_functions_into_transform/Client1.java
@@ -3,15 +3,15 @@ package me.whiteship.refactoring._06_mutable_data._22_combine_functions_into_tra
 import java.time.Month;
 import java.time.Year;
 
-public class Client1 {
+public class Client1 extends ReadingClient{
 
     double baseCharge;
 
     public Client1(Reading reading) {
-        this.baseCharge = baseRate(reading.month(), reading.year()) * reading.quantity();
+        this.baseCharge = enrichReading(reading).baseCharge();
     }
 
-    private double baseRate(Month month, Year year) {
+    protected double baseRate(Month month, Year year) {
         return 10;
     }
 
