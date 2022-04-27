18196e3b847f9f9dcb8a301f68ad9e96ea28cf61
yoonmin-kim <rladbsals23@gmail.com>
2022-04-27 22:04:33 +0900

    refactor : 단계 쪼개기, Split Phase
    · 서로 다른 일을 하는 코드를 각기 다른 모듈로 분리한다
     · 그래야 어떤 것을 변경해야 할 때, 그것과 관련있는 것만 신경쓸 수 있다
    · 여러 일을 하는 함수의 처리 과정을 각기 다른 단계로 구분할 수 있다
     · 예)전처리 -> 주요작업 -> 후처리
     · 예)컴파일러: 텍스트 읽어오기 -> 실행 가능한 형태로 변경
    · 서로 다른 데이터를 사용한다면 단계를 나누는데 있어 중요한 단서가 될 수 있다
    · 중간 데이터(intermediate Data)를 만들어 단계를 구분하고 매개변수를 줄이는데
    활용할 수 있다

---
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceData.java
@@ -0,0 +1,1 @@
+package me.whiteship.refactoring._07_divergent_change._24_split_phase;public record PriceData(int quantity, double basePrice, double discount) {}
--- src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
+++ src/main/java/me/whiteship/refactoring/_07_divergent_change/_24_split_phase/PriceOrder.java
@@ -3,13 +3,21 @@ package me.whiteship.refactoring._07_divergent_change._24_split_phase;
 public class PriceOrder {
 
     public double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
+        final PriceData priceData = calculatePriceData(product, quantity);
+        return applyShipping(priceData, shippingMethod);
+    }
+
+    private PriceData calculatePriceData(Product product, int quantity) {
         final double basePrice = product.basePrice() * quantity;
         final double discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
-        final double shippingPerCase = (basePrice > shippingMethod.discountThreshold()) ?
+        return new PriceData(quantity, basePrice, discount);
+    }
+
+    private double applyShipping(PriceData priceData, ShippingMethod shippingMethod) {
+        final double shippingPerCase = (priceData.basePrice() > shippingMethod.discountThreshold()) ?
                 shippingMethod.discountedFee() : shippingMethod.feePerCase();
-        final double shippingCost = quantity * shippingPerCase;
-        final double price = basePrice - discount + shippingCost;
-        return price;
+        final double shippingCost = priceData.quantity() * shippingPerCase;
+        return priceData.basePrice() - priceData.discount() + shippingCost;
     }
 }
