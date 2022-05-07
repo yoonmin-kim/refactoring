ab3279a1fba762d42a957d2b4447a51a8b0e5ddc
yoonmin-kim <rladbsals23@gmail.com>
2022-05-07 22:13:42 +0900

    refactor : 타입 코드를 서브클래스로 바꾸기, Replace Type Code with Subclasses
    · 비슷하지만 다른 것들을 표현해야 하는 경우, 문자열(String), 열거형(enum), 숫자(int)
     등으로 표현하기도 한다
     · 예) 주문타입, "일반주문", "빠른주문"
     · 예) 직원타입, "엔지니어", "매니저", "세일즈"
    · 타입을 서브클래스로 바꾸는 계기
     · 조건문을 다형성으로 표현할 수 있을 때, 서브클래스를 만들고 "조건부 로직을 다형성으로 바꾸기"
      를 적용한다
     · 특정 타입에만 유효한 필드가 있을 때, 서브클래스를 만들고 "필드 내리기" 를 활용한다

---
+++ src/main/java/me/whiteship/refactoring/_11_primitive_obsession/_31_replace_type_code_with_subclasses/direct_inheritance/Manager.java
@@ -0,0 +1,1 @@
+package me.whiteship.refactoring._11_primitive_obsession._31_replace_type_code_with_subclasses.direct_inheritance;public class Manager extends Employee{	public Manager(String name, String type) {		super(name, type);	}	@Override	public String getType() {		return "manager";	}}
