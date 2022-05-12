38051a6170154675746f9f317206adbc321ad4c8
yoonmin-kim <rladbsals23@gmail.com>
2022-05-12 22:02:01 +0900

    refactor : 죽은 코드 제거하기, Remove Dead Code
    · 사용하지 않는 코드가 애플리케이션 성능이나 기능에 영향을 끼치지는 않는다
    · 하지만, 해당 소프트웨어가 어떻게 동작하는지 이해하려는 사람들에게는 꽤
     고통을 줄 수 있다
    · 실제로 나중에 필요해질 코드라 하더라도 지금 쓰이지 않는 코드라면(주석으로
     감싸는게 아니라) 삭제해야 한다
    · 나중에 정말로 다시 필요해진다면 git과 같은 버전 관리 시스템을 사용해 복원할
     수 있다

--- src/main/java/me/whiteship/refactoring/_15_speculative_generality/_35_remove_dead_code/Reservation.java
+++ src/main/java/me/whiteship/refactoring/_15_speculative_generality/_35_remove_dead_code/Reservation.java
@@ -4,18 +4,12 @@ import java.time.LocalDateTime;
 
 public class Reservation {
 
-    private String title;
-
     private LocalDateTime from;
-
-    private LocalDateTime to;
 
     private LocalDateTime alarm;
 
     public Reservation(String title, LocalDateTime from, LocalDateTime to) {
-        this.title = title;
         this.from = from;
-        this.to = to;
     }
 
     public void setAlarmBefore(int minutes) {
